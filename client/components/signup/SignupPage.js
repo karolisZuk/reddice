c7eaf3e91366c5dd3687bb8948625428f25027c4
Karolis <kar.zukas@gmail.com>
2018-03-30 00:07:10 +0300

    sign up form and its state

--- client/components/signup/SignupPage.js
+++ client/components/signup/SignupPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import SignupForm from './SignupForm';
 
 class SignupPage extends React.Component {
     render(){
     return (
         <div className="container">
-            <h1>Sign up</h1>
+        <div className="row">
+            <div className="col-md-4 offset-md-4">
+                <SignupForm />
+            </div>
+        </div>
         </div>
     );
 }
