ca658e23fce3c0a8af56fff8a6cbe5d1f605cf6f
Karolis <kar.zukas@gmail.com>
2018-03-30 09:37:44 +0300

    redux installed

--- client/index.js
+++ client/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
 
-render(<App />, document.getElementById('app'));
+const store = createStore (
+    (state={}) => state,
+    applyMiddleware(thunk)
+);
+
+render(
+    <Provider store={store} >
+    <App />
+    </Provider>, document.getElementById('app'));
