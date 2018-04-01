da6fe5adca3c7504f4ed8849340562f8100db92d
Karolis <kar.zukas@gmail.com>
2018-04-01 15:33:51 +0300

    use jwt on client and set current user in redux

--- client/index.js
+++ client/index.js
@@ -5,12 +5,21 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
+import setAuthorizationToken from './utils/setAuthorizationToken';
+import { setCurrentUser } from './actions/authActions';
+import jwt from 'jsonwebtoken';
 
 const store = createStore (
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(thunk)
 );
+
+//find auth token is local storage if page is refreshed and set redux state to it
+if(localStorage.jwtToken){
+    setAuthorizationToken(localStorage.jwtToken);
+    store.dispatch(setCurrentUser(jwt.decode(localStorage.jwtToken)));
+}
 
 render(
     <Provider store={store} >
